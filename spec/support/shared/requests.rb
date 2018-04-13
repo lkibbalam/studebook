101c5f3a2222beeec640333123adfc4a3d8a1c91
Dmitriy Gryn <lkibbalam@lkibbalam-lx.nixsolutions.com>
2018-04-13 16:09:15 +0300

    drop some shared

--- spec/support/shared/requests.rb
+++ spec/support/shared/requests.rb
@@ -9,11 +9,3 @@ shared_examples_for 'resource contain' do
     expect(JSON.parse(response.body).size).to eq 10
   end
 end
-
-shared_examples_for 'resource attributes' do
-  subject.each do |attr|
-    it "user object contains #{attr}" do
-      expect(response.body).to be_json_eql(user.send(attr.to_sym).to_json).at_path(attr)
-    end
-  end
-end
