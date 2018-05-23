d7cb21d384f59475e13379e6ea55dbd0887e606e
Dmitriy Gryn <lkibbalam@lkibbalam-lx.nixsolutions.com>
2018-05-23 18:47:20 +0300

    adds send email after create notification to mentor

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,8 +3,9 @@
 class NotificationMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def notification_email(mentor, padawan, task)
-    @task = task
-    mail(to: mentor.email, subject: "Your padawan #{padawan.first_name padawan.last_name} waiting for task approve")
+  def notification_email(mentor, task_user)
+    @task_user = task_user
+    @padawan = task_user.user
+    mail(to: mentor.email, subject: "Your padawan #{@padawan.first_name @padawan.last_name} waiting for task approve")
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,4 +5,12 @@ class Notification < ApplicationRecord
   belongs_to :tasks_user
 
   enum status: { unseen: 0, seen: 1 }
+
+  after_create :send_email_to_mentor
+
+  private
+
+  def send_email_to_mentor
+    NotificationMailer.notification_email(user, tasks_user)
+  end
 end
