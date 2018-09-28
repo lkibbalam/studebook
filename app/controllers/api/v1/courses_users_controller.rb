58f76e4a3e0e7e9c0c3dbb6e61303c38ab68718b
Dmitriy Gryn <lkibbalam@lkibbalam-lx.nixsolutions.com>
2018-09-28 19:49:44 +0300

    Refactor course_user show action

--- app/controllers/api/v1/courses_users_controller.rb
+++ app/controllers/api/v1/courses_users_controller.rb
@@ -3,8 +3,7 @@
 module Api
   module V1
     class CoursesUsersController < ApplicationController
-      before_action :load_user, only: %i[padawan_courses]
-      before_action :load_course_user, only: %i[show]
+      before_action :load_padawan, only: %i[padawan_courses]
 
       def index
         @courses_user = current_user.courses_users
@@ -13,24 +12,27 @@ module Api
       end
 
       def show
-        authorize @course_user
-        respond_with(@course_user)
+        @course_user = current_user.courses_users.find_by(course_id: params[:id])
+        if @course_user
+          authorize @course_user
+          respond_with(@course_user)
+        else
+          @course = Course.find(params[:id])
+          authorize @course
+          respond_with(@course)
+        end
       end
 
       def padawan_courses
-        @courses_user = @user.courses_users
+        @courses_user = @padawan.courses_users
         # authorize @courses_user
         respond_with(@courses_user)
       end
 
       private
 
-      def load_user
-        @user = User.find(params[:id])
-      end
-
-      def load_course_user
-        @course_user = current_user.courses_users.find_by(course_id: params[:id])
+      def load_padawan
+        @padawan = User.find(params[:id])
       end
     end
   end
