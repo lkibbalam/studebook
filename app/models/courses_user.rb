fe4c9e8e936c5a67a09efe919d3f874500c6d93f
Dmitriy Gryn <lkibbalam@lkibbalam-lx.nixsolutions.com>
2018-11-09 14:28:40 +0200

    refactor course user

--- app/models/courses_user.rb
+++ app/models/courses_user.rb
@@ -10,18 +10,24 @@ class CoursesUser < ApplicationRecord
   enum status: { current: 0, archived: 1 }
 
   after_create :create_course_lessons
-  after_update :full_progress_for_archived
+  after_update :full_progress!, if: :course_status_from_current_to_archived?
 
   private
 
   def create_course_lessons
     course.lessons.each { |lesson| student.lessons_users.create(lesson: lesson) }
+    unlock_first_course_lesson
+  end
+
+  def unlock_first_course_lesson
     student.lessons_users.find_by(lesson: course.lessons.order('id').first).update(status: :unlocked)
   end
 
-  def full_progress_for_archived
-    return unless saved_change_to_attribute?('status', from: 'current', to: 'archived')
+  def course_status_from_current_to_archived?
+    saved_change_to_status?(from: 'current', to: 'archived')
+  end
 
+  def full_progress!
     update(progress: 100)
   end
 end
