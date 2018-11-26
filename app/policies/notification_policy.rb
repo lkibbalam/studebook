35ebb34b6ac84287dcdadbb67ec8c129db094536
Dmitriy Gryn <lkibbalam@lkibbalam-lx.nixsolutions.com>
2018-11-26 19:57:20 +0200

    add visitor spec notification policy

--- app/policies/notification_policy.rb
+++ app/policies/notification_policy.rb
@@ -2,11 +2,15 @@
 
 class NotificationPolicy < ApplicationPolicy
   def update?
+    return unless user&.active?
+
     user.admin? || user == record.tasks_user.user.mentor || user == record.tasks_user.user
   end
 
   class Scope < Scope
     def resolve
+      return [] unless user&.active?
+
       scope
     end
   end
---
+++ spec/policies/notification_policy/visitor_context_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe NotificationPolicy do
+  subject { described_class.new(visitor, notification) }
+
+  let(:visitor) { nil }
+  let(:resolved_scope) do
+    described_class::Scope.new(visitor, Notification.all).resolve
+  end
+
+  context 'visitor accessing actions' do
+    let(:notification) { create(:notification) }
+
+    it 'should resolve be empty' do
+      expect(resolved_scope).to be_empty
+    end
+
+    it { is_expected.to forbid_actions(%i[update]) }
+  end
+end
