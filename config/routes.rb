54b685dfa1dba467c304aba8c2fc4e3d4a0da727
Dmitrii1994 <grin-1994@mail.com>
2019-01-09 01:28:34 +0200

    change host default

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   post '/graphql', to: 'graphql#execute'
-  default_url_options host: 'http://localhost:8001'
+  default_url_options host: 'https://studypet.herokuapp.com'
 
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
 
