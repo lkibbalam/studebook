a223e58d57425b016e4a7f6fcb4d875c241fedfb
lkibbalam <lkibbalamlx@gmail.com>
2019-07-27 23:30:44 +0300

    disable redirect ssl

--- app/graphql/types/lesson_user_type.rb
+++ app/graphql/types/lesson_user_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class LessonUserType < BaseObject
+    guard ->(lesson_user, _args, ctx) { LessonsUserPolicy.new(ctx[:me], lesson_user.object).show? }
     field :id, ID, null: false
     field :user, UserType, null: false
     field :lesson, LessonType, null: false
